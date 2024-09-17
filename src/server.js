266ff21b78c39dcc510e6a79961330c6c45c29ce
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2024-09-17 19:48:33 -0400

    adding server.js

---
+++ src/server.js
@@ -0,0 +1,12 @@
+// import http from 'http';
+
+// const PORT = 8000;
+
+// const server = http.createServer((req, res) => {
+//   res.write('Hello World');
+//   res.end('Ended');
+// });
+
+// server.listen(8000, () => {
+//   console.log(`Server running on port${PORT}`);
+// });
