6c0889082988b6e36369d00a2642eb2ca1516309
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2024-09-17 19:48:33 -0400

    adding server.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const historyApiFallback = require('connect-history-api-fallback');
 const path = require('path');
 
 module.exports = {
@@ -7,11 +8,31 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
   module: {
     rules: [
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
     ],
   },
